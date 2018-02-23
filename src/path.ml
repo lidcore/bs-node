54dac7f688d1b903f183f1d2fd3e44182e7f64c0
Romain Beauxis <toots@rastageeks.org>
2018-02-23 09:11:47 -0600

    Simplify.

--- src/path.ml
+++ src/path.ml
@@ -1,12 +1,2 @@
-class type _path = object
-  method dirname : string -> string
-  method normalize : string -> string
-end [@bs]
-
-type path = _path Js.t
-
-external path : path = "" [@@bs.module]
-
-let dirname p = path##dirname p
-
-let normalize p = path##normalize p
+external dirname : string -> string = "" [@@bs.module "path"]
+external normalize : string -> string = "" [@@bs.module "path"]
