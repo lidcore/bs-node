7b8e85dd405a4cf456906eeee66fe27725c5002a
Romain Beauxis <toots@rastageeks.org>
2018-03-07 09:34:20 -0600

    Added os.cpus()

--- src/os.ml
+++ src/os.ml
@@ -1,2 +1,15 @@
+type cpu = <
+  model: string;
+  speed: int;
+  times: <
+    user: int;
+    nice: int;
+    sys:  int;
+    idle: int;
+    irq:  int
+  > Js.t
+> Js.t
+
+external cpus : unit -> cpu array = "" [@@bs.module "os"]
 external platform : unit -> string = "" [@@bs.module "os"]
 external hostname : unit -> string = "" [@@bs.module "os"]
--- src/os.mli
+++ src/os.mli
@@ -1,2 +1,15 @@
+type cpu = <
+  model: string;
+  speed: int;
+  times: <
+    user: int;
+    nice: int;
+    sys:  int;
+    idle: int;
+    irq:  int
+  > Js.t
+> Js.t
+
+val cpus     : unit -> cpu array
 val hostname : unit -> string
 val platform : unit -> string
