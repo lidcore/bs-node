0720dc0e4957987a11e430842fd50de28e4cd088
Neil Mock <neilmock@gmail.com>
2018-05-24 20:07:03 +0000

    take a stab at execFile

--- src/child_process.ml
+++ src/child_process.ml
@@ -9,4 +9,25 @@ let exec cmd cb =
 external execSync : string -> Buffer.t = "" [@@bs.module "child_process"]
 
 let execSync cmd =
-  Buffer.toString (execSync cmd) 
+  Buffer.toString (execSync cmd)
+
+type execFileOptions =
+  {
+    cwd: string[@bs.optional ];
+    env: string Js.Dict.t[@bs.optional ];
+    encoding: string[@bs.optional ];
+    timeout: int[@bs.optional ];
+    maxBuffer: int[@bs.optional ];
+    killSignal: string[@bs.optional ];
+    uid: int[@bs.optional ];
+    gid: int[@bs.optional ];
+    windowsHide: bool[@bs.optional ];
+    windowsVerbatimOptions: bool[@bs.optional ];
+    shell: string[@bs.optional ];}[@@bs.deriving abstract]
+
+external execFile :
+  string -> string array -> execFileOptions -> 'a callback -> unit = "" [@@bs.module "child_process"]
+
+let execFile cmd args options cb =
+  execFile cmd args options (fun [@bs] err stdout stderr ->
+      cb err (stdout, stderr) [@bs])
--- src/child_process.mli
+++ src/child_process.mli
@@ -1,2 +1,5 @@
+type execFileOptions
+
 val exec : string -> (string*string) BsCallback.t
 val execSync : string -> string
+val execFile: string -> string array -> execFileOptions -> (string*string) BsCallback.t
