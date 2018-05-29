382b37b4b513bd0acef7671e1793a4b114653f2d
Neil Mock <neilmock@gmail.com>
2018-05-29 14:08:44 +0000

    changes per PR review

--- src/child_process.ml
+++ src/child_process.ml
@@ -28,8 +28,8 @@ type execFileOptions =
 external execFile :
   string -> string array -> execFileOptions -> 'a callback -> unit = "" [@@bs.module "child_process"]
 
-let execFile cmd args ?cwd ?env ?encoding ?timeout ?maxBuffer ?killSignal ?uid ?gid ?windowsHide ?windowsVerbatimOptions ?shell cb =
-  let options = execFileOptions ?cwd:cwd ?env:env ?encoding:encoding ?timeout:timeout ?maxBuffer:maxBuffer ?killSignal:killSignal ?uid: uid ?gid:gid ?windowsHide: windowsHide ?windowsVerbatimOptions:windowsVerbatimOptions ?shell:shell ()
+let execFile ?cwd ?env ?encoding ?timeout ?maxBuffer ?killSignal ?uid ?gid ?windowsHide ?windowsVerbatimOptions ?shell cmd args cb =
+  let options = execFileOptions ?cwd ?env ?encoding ?timeout ?maxBuffer ?killSignal ?uid ?gid ?windowsHide ?windowsVerbatimOptions ?shell ()
   in
   execFile cmd args options (fun [@bs] err stdout stderr ->
       cb err (stdout, stderr) [@bs])
