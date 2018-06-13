27a7fcdd2030514c0dd3a45bfbe65839b8c4fffb
Neil Mock <neilmock@gmail.com>
2018-06-13 15:19:33 +0000

    attempt to bring execFile into master

--- src/child_process.ml
+++ src/child_process.ml
@@ -9,7 +9,30 @@ let exec cmd cb =
 external execSync : string -> Buffer.t = "" [@@bs.module "child_process"]
 
 let execSync cmd =
-  Buffer.toString (execSync cmd) 
+  Buffer.toString (execSync cmd)
+
+type execFileOptions =
+  {
+    cwd: string[@bs.optional ];
+    env: string Js.Dict.t[@bs.optional ];
+    encoding: string[@bs.optional ];
+    timeout: int[@bs.optional ];
+    maxBuffer: float[@bs.optional ];
+    killSignal: string[@bs.optional ];
+    uid: int[@bs.optional ];
+    gid: int[@bs.optional ];
+    windowsHide: bool[@bs.optional ];
+    windowsVerbatimOptions: bool[@bs.optional ];
+    shell: string[@bs.optional ];}[@@bs.deriving abstract]
+
+external execFile :
+  string -> string array -> execFileOptions -> 'a callback -> unit = "" [@@bs.module "child_process"]
+
+let execFile ?cwd ?env ?encoding ?timeout ?maxBuffer ?killSignal ?uid ?gid ?windowsHide ?windowsVerbatimOptions ?shell cmd args cb =
+  let options = execFileOptions ?cwd ?env ?encoding ?timeout ?maxBuffer ?killSignal ?uid ?gid ?windowsHide ?windowsVerbatimOptions ?shell ()
+  in
+  execFile cmd args options (fun [@bs] err stdout stderr ->
+      cb err (stdout, stderr) [@bs])
 
 type spawned_io = <
   stdin  : Stream.writable;
--- src/child_process.mli
+++ src/child_process.mli
@@ -2,6 +2,7 @@ open BsAsyncMonad
 
 val exec : string -> (string*string) Callback.t
 val execSync : string -> string
+val execFile : string -> string array -> ?cwd:string -> ?env:string Js.Dict.t -> ?encoding:string -> ?timeout:int -> ?maxBuffer:float -> ?killSignal:string -> ?uid:int -> ?gid:int -> ?windowsHide:bool -> ?windowsVerbatimOptions:bool -> ?shell:string -> (string*string) Callback.t
 
 type spawned_io = <
   stdin  : Stream.writable;
